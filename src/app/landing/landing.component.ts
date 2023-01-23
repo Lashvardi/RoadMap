bd807ad347cfdba3df826411684a433acd14704b
Lasha <lashakakashvili114@gmail.com>
2023-01-23 13:38:56 +0400

    Responsivness 2.0

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -13,66 +13,58 @@ export class LandingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
-
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(
-        50,
-        800/600,
-        0.01,
-        5000
-    );
-    var renderer = new THREE.WebGLRenderer({alpha:true});
-    renderer.setSize(800,700)
+    var container = document.getElementById("gia") as HTMLDivElement;
+    var width = container.clientWidth;
+    var height = container.clientHeight;
+    var camera = new THREE.PerspectiveCamera(50, width / height, 0.01, 5000);
+    var renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(width, height);
 
-    document.getElementById("gia")?.appendChild(renderer.domElement)
-    
-
-
+    container.appendChild(renderer.domElement);
 
     var obj: THREE.Group;
     var loader = new GLTFLoader();
-    loader.load("../../assets/scene.gltf",function(gltf){
+    loader.load("../../assets/scene.gltf", function (gltf) {
         obj = gltf.scene;
         scene.add(gltf.scene);
     });
-    renderer.setClearColor( 0x000000, 0 );
+    renderer.setClearColor(0x000000, 0);
 
-    // var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 3);
-    // var light2 = new THREE.PointLight(0xffffff, 3);
     var light3 = new THREE.AmbientLight(0xffffff, 0.7);
     var light4 = new THREE.AmbientLight(0xffffff, 0.9);
-    light4.position.set(4,10,4)
-    scene.add(light3, light4)
-    
+    light4.position.set(4, 10, 4);
+    scene.add(light3, light4);
 
-   const controls = new OrbitControls( camera, renderer.domElement );
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.panSpeed = 1;
+    controls.rotateSpeed = 0.3;
+    controls.enablePan = false;
+    controls.enableDamping = true;
+    controls.enableZoom = false;
+    controls.enabled = true;
 
-   controls.panSpeed = 1;
-   controls.rotateSpeed = 0.3;
-   controls.enablePan = false;
-   controls.enableDamping = true;
-   controls.enableZoom = false
-
-controls.enabled=true;
-
-
-    camera.position.x = 1.4;
+    camera.position.x = 1.6;
     camera.position.y = 0.7;
     camera.position.z = 0;
+    camera.rotation.set(30, 80, 0);
 
-    camera.rotation.set(30, 80, 0)
-    
+    window.addEventListener("resize", () => {
+      width = container.clientWidth;
+      height = container.clientHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+  });
 
-    function animate(){
-
+    function animate() {
         controls.update();
         requestAnimationFrame(animate);
         obj.rotation.y += 0.001;
-        renderer.render(scene,camera);
+        renderer.render(scene, camera);
     }
 
-    animate()
-  }
+    animate();
+}
 
 }
